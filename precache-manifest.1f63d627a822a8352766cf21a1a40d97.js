adfbd7cff9ff832770b63189d6750c2a705e16a9
Will Martino <1953196+buckie@users.noreply.github.com>
2022-01-13 10:27:15 +0100

    pushing new gh-pages build
    Signed-off-by: Will Martino <1953196+buckie@users.noreply.github.com>

---
+++ precache-manifest.1f63d627a822a8352766cf21a1a40d97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e61c605008a24c10113eace4f4b8173",
+    "url": "/marmalade/index.html"
+  },
+  {
+    "revision": "40d0011b5d259c0d8bad",
+    "url": "/marmalade/static/css/2.199d4a19.chunk.css"
+  },
+  {
+    "revision": "40d0011b5d259c0d8bad",
+    "url": "/marmalade/static/js/2.b5bf569b.chunk.js"
+  },
+  {
+    "revision": "5b691bc977ca7f537343ee6205f18cc9",
+    "url": "/marmalade/static/js/2.b5bf569b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83cf76ccf27d6d41f2a7",
+    "url": "/marmalade/static/js/main.67a88931.chunk.js"
+  },
+  {
+    "revision": "06ab33c150f01673fc0c",
+    "url": "/marmalade/static/js/runtime-main.646b119b.js"
+  },
+  {
+    "revision": "184cc2aa160399bbfed61974ef5e2479",
+    "url": "/marmalade/static/media/kadena_r_rev_3_whi_lor.184cc2aa.png"
+  }
+]);
